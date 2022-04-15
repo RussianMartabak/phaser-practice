d193d0cd121b77f452f4ae419b8028e23d598d3a
Zaky <zakyabdurrahman10@gmail.com>
2022-04-15 16:06:35 +0700

    load asset and display sky background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
-
+import sky from './assets/sky.png';
+import ground from './assets/platform.png';
+import spriteSheetDude from './assets/dude.png';
 
 
 var config = {
@@ -19,10 +21,22 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     //loading assets
+    this.load.image('sky', sky);
+    this.load.image('ground', ground);
+    this.load.spritesheet('dude',
+    spriteSheetDude,
+    {
+        frameWidth: 32,
+        frameHeight: 48
+    }
+    )
 }
 
 function create ()
-{
+{   
+    //putting asset
+    this.add.image(400, 300, 'sky');//x, y, name of image
+
 }
 
 function update ()
